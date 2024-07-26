2cfc534e149dd2da4d619aef185997d375ad0a00
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2024-07-26 06:57:59 -0700

    updated some files

---
+++ notebook.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+class CarDekho:
+    """CarDekho 2020 Data Analysis and Predictions"""
+    def __init__(self,sales):
+        self.sales=sales
+    def data(sales):
+        sales = pd.read_csv('data/cars_dataset.csv')
+        print(sales.head())
+        print(sales.describe())
+    def mode
