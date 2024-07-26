f5db461de46e9735ededa42b185a5984d543f80b
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2024-07-26 08:57:12 -0700

    did some updates

--- notebook.py
+++ notebook.py
@@ -1,19 +1,28 @@
 import numpy as np
 import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from sklearn.linear_model import LinearRegression
-import tensorflow as tf
 # from torch import *
 # from keras import *
-from tensorflow.keras import Sequential
-class CarDekho:
-    """CarDekho 2020 Data Analysis and Predictions"""
-    def __init__(self,sales):
-        self.sales=sales
-    def data(sales):
-        sales = pd.read_csv('data/cars_dataset.csv')
-        print(sales.head())
-        print(sales.describe())
-    def model(model):
-        model.sales_model=sales_model
-        
+try:
+    # class CarDekho:
+    #     """CarDekho 2020 Data Analysis and Predictions"""
+    #     def __init__(self,sales):
+    #         self.sales=sales
+    #     def data(sales):
+    #         sales = pd.read_csv('data/cars_dataset.csv')
+    #         print(sales.head())
+    #         print(sales.describe())
+    #     def model(model):
+    #         model.sales_model=sales_model
+    sales=pd.read_csv('data/cars_dataset.csv')
+    # print(sales.describe())
+    # print(sales.head())
+    print(sales)
+    # y=sales
+
+except:
+    # import tensorflow as tf
+    # from tensorflow.keras import Sequential
+    # from tensorflow.keras.layers import Dense
+    sales=pd.read_csv('data/cars_dataset.csv')
+    sales.describe();
+    # model=Sequential([Dense(units=1)])    
